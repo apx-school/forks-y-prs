bfc2396c05e9f00495da40551fa3ad24d7520dfb
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-03-16 21:28:11 -0300

    challenge code

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
+import * as products from "./products.json";
+
 class Product {
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
+  id: number;
   name: string;
   price: number;
 }
@@ -15,6 +18,9 @@ class User {
   products: Product[] = [];
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
+  }
+  addProducts(newProducts: Product[]) {
+    this.products.push(newProducts);
   }
 }
 
