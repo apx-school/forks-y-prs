2626688df8bde23752ea1359aa1cdb90275f699a
Monchi <conejobohemio@gmail.com>
2021-03-25 10:53:05 -0300

    desafío completado

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase: number) {
+    const productosFiltrados = products.filter((i) => i.price < precioBase);
+    return productosFiltrados;
+  }
 }
 
 class User {
@@ -20,7 +25,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
