23b7a8160c039c0cda9a32d756c8daa5056a461a
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-05-24 20:49:55 -0300

    Commiteo

--- index.ts
+++ index.ts
@@ -8,6 +8,13 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow (precioBase:number) {
+    const productosEncontrados = products.filter((p) => {
+      return p.price < precioBase
+    })
+    return productosEncontrados
+  }
 }
 
 class User {
@@ -20,7 +27,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat (newProducts);
   }
 }
 
