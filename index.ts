4e619f7318687c36bf82bbf8d8fe250e7c7f5914
Nehuen15 <nehuencarrizo@outlook.com.ar>
2021-06-05 12:37:43 -0300

    Arreglado addProducts y agregado findProductsBelow

--- index.ts
+++ index.ts
@@ -1,27 +1,34 @@
 import * as products from "./products.json";
-
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
+    id: number;
+    name: string;
+    price: number;
+    static findProductsBelow(precioBase: number) {
+        var productoBaratos = products.filter((objProduct) => {
+            return objProduct.price < precioBase;
+        });
+        return productoBaratos;
+    }
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
-  }
+    constructor(name: string) {
+        this.name = name;
+    }
+    name: string;
+    products: Product[] = [];
+    addProduct(newProduct: Product) {
+        this.products.push(newProduct);
+    }
+    addProducts(newProducts: Product[]) {
+        newProducts.map((producto) => {
+            this.products.push(producto);
+        });
+    }
 }
 
 export { User, Product };
