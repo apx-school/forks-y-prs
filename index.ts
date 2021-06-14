cd3552a4f48982d62b2d6967221cd3c22d7bc541
Facundo Arias <facundolautaroarias@hotmail.com>
2021-06-14 15:25:09 -0300

    class User y metodo addProductsBelow FIXED

--- index.ts
+++ index.ts
@@ -1,26 +1,34 @@
 import * as products from "./products.json";
 
 class Product {
+  id: number;
+  name: string;
+  price: number;
+
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
-  id: number;
-  name: string;
-  price: number;
+
+  static findProductsBelow(price: number) {
+    let productsBelow = products.filter((x) => x.price < price);
+    return productsBelow;
+  }
 }
 
 class User {
+  name: string;
+  products: Product[] = [];
+
   constructor(name: string) {
     this.name = name;
   }
-  name: string;
-  products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
