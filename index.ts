043b7e3106641bbc8297c0b4979815c5bfec7cb9
Javier <javier1322@gmail.com>
2021-06-16 20:33:02 -0300

    modify index.ts

--- index.ts
+++ index.ts
@@ -8,6 +8,10 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase: number) {
+    return products.filter((item) => item.price < precioBase);
+  }
 }
 
 class User {
@@ -20,7 +24,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products.push(...newProducts);
   }
 }
 
