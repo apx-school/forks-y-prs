8a4f80fab8a995060d03d9b4f06e587fef395233
alayn saez <saezalayn@gmail.com>
2021-06-20 15:45:02 -0300

    repaso

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precio: number) {
+    return products.filter((i) => {
+      return i.price < precio;
+    });
+  }
 }
 
 class User {
@@ -20,7 +26,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
