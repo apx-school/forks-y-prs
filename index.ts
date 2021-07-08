4d9755b63491a8d002fea1f76af1ac60218490b4
Leandro <leandrom.roldan@gmail.com>
2021-07-07 21:09:27 -0300

    hice las correcciones en los métodos

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+  static findProductsBelow(precioBase:number){
+    const productosEncontrados = products.filter(item =>{
+      return item.price < precioBase;
+    })
+    return productosEncontrados;
+  }
 }
 
 class User {
@@ -20,7 +26,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
