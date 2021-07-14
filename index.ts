ec967b08e6a3539cb1a856edc8d787c13ddba804
Agustin <Agustin@MacBook-Air-de-Agustin.local>
2021-07-14 19:44:06 -0300

    hice el desafio :)

--- index.ts
+++ index.ts
@@ -6,39 +6,16 @@ class Product {
     this.name = name;
     this.price = price;
   }
+  
+  static findProductsBelow(precioBase: number) {
+    const nuevoArray = products.filter(p=>{
+      return p.price > precioBase
+    })
+    return nuevoArray
+  }  
   id: number;
   name: string;
   price: number;
-
-  static findProductsBelow(precioBase: number) {
-    const products0 = products[0].price
-    const products1 = products[1].price
-    const products2 = products[2].price
-    const products3 = products[3].price
-    const products4 = products[4].price
-
-    const name0 = products[0].name
-    const name1 = products[1].name
-    const name2 = products[2].name
-    const name3 = products[3].name
-    const name4 = products[4].name  
-
-
-    if (products0 < precioBase)
-    return name0
-
-    if (products1 < precioBase)
-    return name1
-
-    if (products2 < precioBase)
-    return name2
-
-    if (products3 < precioBase)
-    return name3
-
-    if (products4 < precioBase)
-    return name4
-  }
 }
 
 class User {
@@ -53,8 +30,8 @@ class User {
     this.products.push(newProduct);
   }
 
-  addProducts(newProducts: Product) {
-    this.products.push(newProducts);
+  addProducts(newProducts: Product []) {
+    this.products = this.products.concat(newProducts);
   }
 }
 
