8d5daee1e693934cbdc453d603c498a345ea42bb
Caro <carofattore@gmail.com>
2021-07-26 20:25:21 -0300

    Agregue un metodo estatico e hice funcionar los tests

--- index.ts
+++ index.ts
@@ -8,6 +8,13 @@ class Product {
   id: number;
   name: string;
   price: number;
+  
+  static findProductsBelow(precioBase:number) {
+    const respuesta = products.filter((p)=>{
+      return p.price < precioBase;
+    });
+    return respuesta;
+  }
 }
 
 class User {
@@ -20,7 +27,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
