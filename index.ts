1fbff8f82c771a3a8cd4d4e89217a91d1e145308
Burrito <cabreragaby09gmail.com>
2021-07-26 19:32:14 -0300

    desafio arreglado

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+  static findProductsBelow(precio: number) {
+    const nuevoArray = products.filter(function (p) {
+      return p.price < precio;
+    });
+    return nuevoArray;
+  }
 }
 
 class User {
@@ -20,7 +26,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
