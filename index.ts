e2a823e5986e2dd3e561731320beba46d80d0290
AndyConty <andresconvar@gmail.com>
2021-07-29 13:16:20 -0300

    arreglé las fallas

--- index.ts
+++ index.ts
@@ -1,27 +1,35 @@
-import * as products from "./products.json";
+/** @format */
+
+import * as products from './products.json';
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
+	constructor(name: string, price: number) {
+		this.name = name;
+		this.price = price;
+	}
+	static findProductsBelow(price: number) {
+		const nuevoArray = products.filter((p) => {
+			return p.price < price;
+		});
+		return nuevoArray;
+	}
+	id: number;
+	name: string;
+	price: number;
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
-  }
+	constructor(name: string) {
+		this.name = name;
+	}
+	name: string;
+	products: Product[] = [];
+	addProduct(newProduct: Product) {
+		this.products.push(newProduct);
+	}
+	addProducts(newProducts: Product[]) {
+		this.products = this.products.concat(newProducts);
+	}
 }
 
 export { User, Product };
