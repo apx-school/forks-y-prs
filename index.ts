a241f1ca064c64dbcb4ba73b7605ce004577b336
Luca <lupattin@gmail.comgit config --global user.email lupattin@gmail.comgit config --global user.email lupattin@gmail.com>
2021-08-22 23:26:45 -0300

    Solucion

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
 import * as products from "./products.json";
 
+
+
 class Product {
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
+ static findProductsBelow(price: number){
+   const newArray = products.filter(function(item){
+     return item.price < price
+   })
+   return newArray
+ }
   id: number;
   name: string;
   price: number;
@@ -20,8 +28,13 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
+const user = new User("luca")
+const product = new Product ("tele", 100)
+user.addProduct(product)
 
+
+console.log(user)
 export { User, Product };
