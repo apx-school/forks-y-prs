a3ee134067380651c00ce5aab4b0281e65303bc4
Juan Sitges Riera <juan.s.riera@gmail.com>
2021-09-18 16:48:18 +0200

    cambios realizados

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase:number):Product[]{
+    return products.filter(function(item){
+      return item.price < precioBase 
+    })
+  }
 }
 
 class User {
@@ -20,7 +26,10 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    newProducts.forEach(element => {
+      this.products.push(element) 
+    });
+    
   }
 }
 
