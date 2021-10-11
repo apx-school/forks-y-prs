b6438dc933c92617d5230ae56bdbdd97a7090ceb
Javier Diaz <diazjavierdavid@hotmail.com>
2021-10-10 21:36:05 -0300

    Fin del ejercicio

--- index.ts
+++ index.ts
@@ -1,26 +1,29 @@
 import * as products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
   id: number;
-  name: string;
-  price: number;
+
+  constructor(public name: string, public price: number) {
+  }
+
+  static findProductsBelow(precioBase: number){
+     const productosEncontrados = products.filter(product => product.price < precioBase);
+     return productosEncontrados;
+  }
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
+  products: Product[]= [];
+
+  constructor(public name: string) {
   }
-  name: string;
-  products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+  
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products.push(...newProducts);
   }
 }
 
