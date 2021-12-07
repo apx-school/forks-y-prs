9146b3dcd475bb214d644aad9ce41ad6c5672811
Tobías Facello <tobiasfacellodeveloper@gmail.com>
2021-12-07 01:38:58 -0300

    Ejercicio resuelto

--- index.ts
+++ index.ts
@@ -1,27 +1,36 @@
 import * as products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
+	constructor(name: string, price: number) {
+		this.name = name;
+		this.price = price;
+	}
+	id: number;
+	name: string;
+	price: number;
+
+	static findProductsBelow(precioBase: number) {
+		const productosBuscados = products.filter((prod) => {
+			if (prod.price < precioBase) {
+				return prod;
+			}
+		});
+		return productosBuscados;
+	}
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
-  }
+	constructor(name: string) {
+		this.name = name;
+	}
+	name: string;
+	products: Product[] = [];
+	addProduct(newProduct: Product) {
+		this.products.push(newProduct);
+	}
+	addProducts(newProducts: Product[]) {
+		newProducts.map((prod) => this.products.push(prod));
+	}
 }
 
 export { User, Product };
