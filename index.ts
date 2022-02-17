13671fec5b3409262f780a1a3b96396e525cbea2
ayrton juarez <ayrtonjuarez90@gmail.com>
2022-02-17 08:59:26 -0300

    arregle addProductos y cree el metodo estatico

--- index.ts
+++ index.ts
@@ -1,27 +1,38 @@
 import * as products from "./products.json";
+export { Product, User };
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
+    constructor(name: string, price: number) {
+        this.name = name;
+        this.price = price;
+    }
+    id: number;
+    name: string;
+    price: number;
+    static findProductsBelow(precioBase: number) {
+        const precioBajo = products.filter((item) => {
+            return item.price < precioBase;
+          });
+          return precioBajo
+        }
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
-  }
+    constructor(name: string) {
+        this.name = name;
+    }
+    name: string;
+    products: Product[] = [];
+    addProduct(newProduct: Product) {
+        this.products.push(newProduct);
+    }
+    addProducts(newProducts: Product[]) {
+        this.products = this.products.concat(newProducts);
+    }
 }
 
-export { User, Product };
+// export { User, Product };
+
+// Agregar el método estático findProductsBelow(precioBase:number)
+// que devuelva los productos (que son importados desde ./products.json)
+// con el precio más bajo que el parámetro precioBase.
