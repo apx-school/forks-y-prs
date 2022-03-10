9230c7f105155962a9cb3d07a00fc1526358d487
Eleri <n.erik.jak@gmail.com>
2022-03-10 18:00:10 -0300

    1er commit

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(pricenumber:number){
+    return products.filter((p:any)=>{
+      return p.price<=pricenumber
+    })
+  }
 }
 
 class User {
@@ -20,8 +26,19 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    Array.prototype.push.apply(this.products,newProducts)
   }
 }
+
+const p1 = new Product("compu", 1000)
+const p2 = new Product("celu", 1500)
+
+const user1 = new User("eleri")
+
+user1.addProducts([p1,p2])
+const constante1=Product.findProductsBelow(250)
+
+console.log(user1)
+console.log(constante1)
 
 export { User, Product };
