cbf96a7790e28744c93cfad320183befcfd22f16
Gabriel González <ggonzalez@omixom.com>
2022-04-06 19:07:35 -0300

    Acomodando las versiones y la compatibilidad de package.json para nuevas versiones de ava, solucionando errores de implementacion pedidas por el desafio del capitulo

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase:number){
+    return products.filter((item) => {
+      return item.price < precioBase;
+    });
+  }
 }
 
 class User {
@@ -20,7 +26,9 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    newProducts.forEach((item)=>{
+      this.products.push(item);
+    });
   }
 }
 
