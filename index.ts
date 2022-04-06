c8d6cabedcf077ca80ca59864b357ca4b277d17c
NahuelAlfaro <98058039+NahuelAlfaro@users.noreply.github.com>
2022-04-06 20:18:15 -0300

    add static findProductsBelow and fix addProducts

--- index.ts
+++ index.ts
@@ -8,6 +8,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+  static findProductsBelow(price:number){
+    const encontrarPrecio = products.filter(p=>{
+      return p.price < price
+    })
+    return encontrarPrecio
+  }
 }
 
 class User {
@@ -20,7 +26,7 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
   }
 }
 
