012f7e08c2bc5b6ff447dad94e7d676105d32c57
Joel jimenez <joelnunez855@gmail.com>
2022-04-26 20:00:23 -0300

    final

--- index.ts
+++ index.ts
@@ -1,19 +1,45 @@
-import * as products from './products.json';
+/*class Peli{
+    constructor(title, tags){
+    this.title=title; 
+    this.tags=tags
+    }
+    title:string
+    tags:string[]
+    getTitle():string{
+        return this.title
+    }
+    static buscarPeli():Peli[]{//entre estos corchetes voy a instanciar una simulacion de "pelis", pero aca cargariamos un json(que esta cargada de dif pelis) por ejemplo.Entonces buscar entre todos ellos y que sea un metodo estatico va a ser de gran ayuda
+const resulltado = new Peli("reina",["comedia","romantica"]);
+return [resulltado]
+    }
+}
+
+function main(){
+  const mipeli= new Peli ("rogelio",["2","2"])
+ console.log(mipeli.getTitle())
+
+const busqueda= Peli.buscarPeli()
+console.log(busqueda[0])
+
+}
+main();*/
+
+import * as products from "./products.json";
 
 class Product {
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
-  static findProductsBelow(precio: number) {
-    const result = products.filter((p) => {
-      return p.price < precio;
-    });
-    return result;
-  }
   id: number;
   name: string;
   price: number;
+
+ static findProductsBelow(precioBase:number){
+   const preciosBajos= products. filter(p=>{
+     return p.price < precioBase 
+   })
+ return preciosBajos}
 }
 
 class User {
@@ -26,7 +52,8 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products = this.products.concat(newProducts);
+    this.products= newProducts.concat(newProducts)// se cambio el push, haciendo un concat para que agregue todos los item.
+
   }
 }
 
