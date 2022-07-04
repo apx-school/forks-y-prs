4ac9b3025b3ea43c3ae05e986e06cd3b5c91ba0a
--replace <cursodeprogramacion@gmail.com>
2022-07-03 22:39:17 -0300

    commit con el error .ts

--- index.ts
+++ index.ts
@@ -1,27 +1,34 @@
 import * as products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
+	constructor(name: string, price: number) {
+		this.name = name;
+		this.price = price;
+	}
+	id: number;
+	name: string;
+	price: number;
+
+	static findProductsBelow(precioBase: number) {
+		const resultado = products.filter((p) => {
+			return p.price < precioBase;
+		});
+		return resultado;
+	}
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
-  }
+	constructor(name: string) {
+		this.name = name;
+	}
+	name: string;
+	products: Product[] = [];
+	addProduct(newProduct: Product) {
+		this.products.push(newProduct);
+	}
+	addProducts(newProducts: Product[]) {
+		return (this.products = this.products.concat(newProducts));
+	}
 }
 
 export { User, Product };
