ac5979afb75d165acb4eceed05b3f44de19f8721
Ricardo <mackaroots@gmail.com>
2023-08-06 17:40:21 -0300

    modificacion de la class User e implementacion de la funcion findProducts

--- index.ts
+++ index.ts
@@ -16,12 +16,28 @@ class User {
   }
   name: string;
   products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+   for(const product of newProducts){
+this.products.push(product)
+   };
+  }
+  static findProductsBelow(precioBase:number){
+    return products.filter((item)=> item.price <= precioBase)
   }
 }
+const product1 = new Product("Laptop", 1000);
+const product2 = new Product("Mouse", 20);
+const pedro = new User('Pedro')
+
+pedro.addProduct(product1)
+
+const newProducts = [product1, product2]
+pedro.addProducts(newProducts)
+
+
 
 export { User, Product };
