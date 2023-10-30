4d5333ef0945faeaa8d27428a2b301b9c3a86d89
Nes <nes2504@hotmail.com>
2023-10-30 13:43:46 -0300

    segundo commit

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
 import * as products from "./products.json";
+import * as fs from "fs";
+
+function getArchivo() {
+  const jsonFile = "./products.json";
+  const archivo = fs.readFileSync(jsonFile, "utf-8");
+  const objeto = JSON.parse(archivo);
+  return objeto;
+}
 
 class Product {
+  static contadorID: number = 1;
   constructor(name: string, price: number) {
+    this.id = Product.contadorID++;
     this.name = name;
     this.price = price;
   }
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precio: number) {
+    const archivo = getArchivo();
+    return archivo.filter((p) => p.price < precio);
+  }
 }
 
 class User {
@@ -20,8 +35,13 @@ class User {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    newProducts.forEach((p) => {
+      this.products.push(p);
+    });
+  }
+  getProducts() {
+    return this.products;
   }
 }
 
-export { User, Product };
+export { User, Product, getArchivo };
--- prueba.ts
+++
@@ -1,1 +0,0 @@
-console.log("es un archivo de prueba");
