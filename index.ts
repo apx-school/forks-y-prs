177e3f9d7dea98bc1c45e8b617318e787c384042
Gonzalo Zaniewski <gonzalozaniewski@outlook.com>
2023-11-22 01:33:22 -0300

    Corregir método addProducts en la clase User

--- index.ts
+++ index.ts
@@ -1,26 +1,38 @@
-import * as products from "./products.json";
-
 class Product {
+  static idCounter = 1;
+
   constructor(name: string, price: number) {
+    this.id = Product.idCounter++;
     this.name = name;
     this.price = price;
   }
+
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(priceBase: number, allProducts: Product[]): Product[] {
+    return allProducts.filter((product) => product.price <= priceBase).map((product) => ({ ...product, id: Product.idCounter++ }));
+  }
 }
+
+
 
 class User {
   constructor(name: string) {
     this.name = name;
   }
+
   name: string;
   products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+
   addProducts(newProducts: Product[]) {
-    this.products.push(newProducts);
+    // Usamos el spread operator (...) para agregar los nuevos productos sin modificar el array original
+    this.products = [...this.products, ...newProducts.filter(product => product instanceof Product)];
   }
 }
 
