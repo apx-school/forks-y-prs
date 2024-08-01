6be2bdf6d2367611d58e9f2eb30d606a809cfc35
Martin <marsebas99@gmail.com>
2024-08-01 02:05:31 -0300

    Solucione findProductsBelow y addProducts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import products from "./products.json";
 
+
+
 class Product {
   constructor(name: string, price: number) {
     this.name = name;
@@ -8,6 +10,12 @@ class Product {
   id: number;
   name: string;
   price: number;
+ static findProductsBelow(price:number){
+  const NuevoArray = products.filter(p=> {
+    return p.price < price;
+  })
+return NuevoArray;
+ }
 }
 
 class User {
@@ -21,9 +29,10 @@ class User {
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+   this.products = this.products.concat(newProducts);
+ return products;
     // pista: push no suma muchos items (agrega de a uno)
-  }
+    }
 }
 
 export { User, Product };
