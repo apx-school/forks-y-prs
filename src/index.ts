4ebc2567cd697b8d4bf0e4e27df731d199c87628
Santia <santiagogmuller@gmail.com>
2024-08-03 23:19:41 -0300

    aprueba test

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ class Product {
   id: number;
   name: string;
   price: number;
+  static findProductsBelow(precioAComparar: number): Product[] {
+     return products.filter((obj) => {
+     return obj.price < precioAComparar
+    });  
+    }
 }
 
 class User {
@@ -21,9 +26,26 @@ class User {
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+   this.products = this.products.concat(newProducts) ; //ACA PODRÍA USARSE METODO CONCAT
     // pista: push no suma muchos items (agrega de a uno)
+    //return products.push(agrega)
   }
 }
+
+
+//const usuario = new User("Tito")
+//console.log (products)
+//console.log(usuario.addProduct(new Product("chizitos", 5)))
+//console.log(Product.findProductsBelow(300))
+
+//const productosNoTanCaros = Product.findProductsBelow(250);
+//console.log(productosNoTanCaros); //ESTO FUNCIONA
+//usuario.addProduct(new Product("chizitos", 50))
+//console.log(usuario) //ESTO FUNCIONA
+//usuario.addProducts(products);//ESTO FUNCIONA
+//console.log(usuario)
+
+  //usuario.addProduct(producto);
+//console.log(usuario.addProducts(productosNoTanCaros));
 
 export { User, Product };
