fd1e3728beb9ddbe63a543b2f9897d594c33d470
Rodrigo Torrico <rodrigotorrico@yahoo.com.ar>
2024-09-24 19:19:15 -0300

    cambios en la clase user y agregamos una funcion a la clase product

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,44 @@
-import products from "./products.json";
+import  products from "./products.json";
 
-class Product {
-  constructor(name: string, price: number) {
+
+
+class Product {  
+constructor(name: string, price: number) {
+    
     this.name = name;
     this.price = price;
   }
+
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(precioBase: number){
+    return products.filter((p) => p.price < precioBase)
+    }
+
 }
 
 class User {
+  
+  name: string;
+  products: Product[] = [];
+  
   constructor(name: string) {
     this.name = name;
   }
-  name: string;
-  products: Product[] = [];
+  
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+  
   addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
+    this.products = this.products.concat(newProducts);
     // pista: push no suma muchos items (agrega de a uno)
   }
+
+  
 }
+
 
 export { User, Product };
