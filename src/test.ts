91b4832f176c39e17239f03d2ad3a5833826eeb9
Ale <ale.villa.0296@gmail.com>
2025-01-19 21:36:03 -0300

    Resolucion

--- src/test.ts
+++ src/test.ts
@@ -2,9 +2,9 @@ import test from "ava";
 import { User, Product } from "./index";
 
 test("Testeo la clase User", (t) => {
-  const user = new User("marce");
+    const user = new User("marce");
 
-  t.is(user.name, "marce");
+    t.is(user.name, "marce");
 });
 
 // El siguiente text va a fallar inicialmente, el objetivo es
@@ -13,18 +13,18 @@ test("Testeo la clase User", (t) => {
 // que debería pasar.
 
 test("Testeo la clase Product y sus métodos", (t) => {
-  const user = new User("marce");
-  const producto = new Product("termo", 500);
+    const user = new User("marce");
+    const producto = new Product("termo", 500);
 
-  // El método estatico findProductsBelow no existe
-  // tenés que hacer que funcione
-  const productosNoTanCaros = Product.findProductsBelow(250);
+    // El método estatico findProductsBelow no existe
+    // tenés que hacer que funcione
+    const productosNoTanCaros = Product.findProductsBelow(250);
 
-  user.addProduct(producto);
-  user.addProducts(productosNoTanCaros);
+    user.addProduct(producto);
+    user.addProducts(productosNoTanCaros);
 
-  t.is(user.products[0].name, "termo");
-  t.is(user.products[0].price, 500);
-  t.is(user.products[1].id, 1);
-  t.is(user.products[2].id, 2);
+    t.is(user.products[0].name, "termo");
+    t.is(user.products[0].price, 500);
+    t.is(user.products[1].id, 1);
+    t.is(user.products[2].id, 2);
 });
