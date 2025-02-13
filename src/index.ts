73b0674fc87f716b32ad927452d9b8b27e366375
nvei <lionelcasao360@gmail.com>
2025-02-13 12:17:49 -0300

    Primer edit al Index, added findProductsBelow()

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
+  constructor(name: string, price: number, id?: number) {
+    this.id = id
     this.name = name;
     this.price = price;
   }
   id: number;
   name: string;
   price: number;
+
+  static findProductsBelow(basePrice: number){
+    return products.filter((product)=> product.price < basePrice )
+  }
+
 }
 
 class User {
@@ -21,7 +27,7 @@ class User {
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    this.products.push(...newProducts);
     // pista: push no suma muchos items (agrega de a uno)
   }
 }
