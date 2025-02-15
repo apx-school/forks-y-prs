585013b6a6159b5ce0d594728b8e881741cbb55e
estefanoquiriconi <estefanoquiriconi@gmail.com>
2025-02-15 18:50:29 -0300

    Refactor Product and User classes and method implementation

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,36 @@
-import products from "./products.json";
+import products from './products.json';
 
 class Product {
+  id: number;
+  name: string;
+  price: number;
+
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
-  id: number;
-  name: string;
-  price: number;
+
+  static findProductsBelow(precioBase: number) {
+    return products.filter((product) => product.price < precioBase);
+  }
 }
 
 class User {
+  name: string;
+  products: Product[] = [];
+
   constructor(name: string) {
     this.name = name;
   }
-  name: string;
-  products: Product[] = [];
+
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
+
   addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
-    // pista: push no suma muchos items (agrega de a uno)
+    for (const product of newProducts) {
+      this.products.push(product);
+    }
   }
 }
 
