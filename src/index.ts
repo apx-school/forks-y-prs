a18e474b3b375b07c5dfcc417243e7fa3a14e09a
JhonatanDzG <jhonatandzg@gmail.com>
2025-02-22 12:24:48 -0500

    feat: index.ts-DONE

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 import products from "./products.json";
 
 class Product {
+  id: number;
+  name: string;
+  price: number;
   constructor(name: string, price: number) {
     this.name = name;
     this.price = price;
   }
-  id: number;
-  name: string;
-  price: number;
+  static findProductsBelow(price: number): Product[] {
+    return products.filter((p) => p.price < price);
+  }
 }
 
 class User {
+  name: string;
+  products: Product[] = [];
   constructor(name: string) {
     this.name = name;
   }
-  name: string;
-  products: Product[] = [];
   addProduct(newProduct: Product) {
     this.products.push(newProduct);
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    this.products.push(...newProducts);
     // pista: push no suma muchos items (agrega de a uno)
   }
 }
