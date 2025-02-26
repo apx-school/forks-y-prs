88c85dfe8e1a5bfc2ae7a32a1ce2092fc8ce8f94
felis <felisicca@gmail.com>
2025-02-26 12:00:17 -0300

    Agregado de metodo findProductsBelow y uso del operador de propagacion en addProducts para pasar el test

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ class Product {
   id: number;
   name: string;
   price: number;
+  static findProductsBelow(precioBase: number): Product[] {
+    return products.filter(p => p.price < precioBase)
+    }
 }
 
 class User {
@@ -21,9 +24,16 @@ class User {
   }
   addProducts(newProducts: Product[]) {
     // esto no funciona:
-    this.products.push(newProducts);
+    this.products.push(...newProducts);
+    // Utilización del operador de propagación (...) para evitar la anidación de arrays,
+    // permitiendo agregar elementos de newProducts a this.products, que es un array existente.
+    // Esto es lo que busca el test.
+    // También se podría usar concat() para combinar elementos de varios arrays sin modificar los originales.
+    // Nota: push puede agregar múltiples elementos a la vez si se usa el operador de propagación.
+    
     // pista: push no suma muchos items (agrega de a uno)
   }
+
 }
 
 export { User, Product };
