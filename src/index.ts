fd8be15358197008afea75418e8b68c55e1042e1
Lucas Martin <lucasmartintorresdiaz@gmail.com>
2025-02-28 19:01:13 -0300

    arregle el método addProducts para que agregue mas de un producto y cree la el método estatico para que muestre la lista de productos de un valor menor o igual a 250

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,33 @@
 import products from "./products.json";
 
 class Product {
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-  id: number;
-  name: string;
-  price: number;
+	constructor(name: string, price: number) {
+		this.name = name;
+		this.price = price;
+	}
+	static findProductsBelow(price: number) {
+		const nuevoArray = products.filter((p) => {
+			return p.price < price;
+		});
+		return nuevoArray;
+	}
+	id: number;
+	name: string;
+	price: number;
 }
 
 class User {
-  constructor(name: string) {
-    this.name = name;
-  }
-  name: string;
-  products: Product[] = [];
-  addProduct(newProduct: Product) {
-    this.products.push(newProduct);
-  }
-  addProducts(newProducts: Product[]) {
-    // esto no funciona:
-    this.products.push(newProducts);
-    // pista: push no suma muchos items (agrega de a uno)
-  }
+	constructor(name: string) {
+		this.name = name;
+	}
+	name: string;
+	products: Product[] = [];
+	addProduct(newProduct: Product) {
+		this.products.push(newProduct);
+	}
+	addProducts(newProducts: Product[]) {
+		this.products = this.products.concat(newProducts);
+	}
 }
 
 export { User, Product };
